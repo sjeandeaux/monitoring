439ccd14a085db997179724dfd027ef606002569
sjeandeaux <stephane.jeandeaux@gmail.com>
2015-02-11 22:00:53 +0100

    centos 7

--- Vagrantfile
+++ Vagrantfile
@@ -14,7 +14,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     monitoring.vm.provider "virtualbox" do |v|
       v.name = "monitoring"
     end
-    monitoring.vm.box = "chef/centos-6.5"
+    monitoring.vm.box = "chef/centos-7.0"
 
   
     #influxdb
@@ -35,7 +35,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     monitoring.vm.synced_folder "box/", "/tmp/monitoring"
 
 
-    monitoring.vm.provision "shell", path: "box/monitoring.sh"
+    #monitoring.vm.provision "shell", path: "box/monitoring.sh"
   
 
   end
--- box/monitoring.sh
+++ box/monitoring.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 FOLDER_CONFIG=/tmp/monitoring/config
 
 INFLUXDB_CONFIG=$FOLDER_CONFIG/influxdb.toml
@@ -9,23 +11,15 @@ GRAFANA_DASH=$FOLDER_CONFIG/dash.json
 NGINX_CONFIG=$FOLDER_CONFIG/nginx_grafana
 STATSD_CONFIG=$FOLDER_CONFIG/statsd.js
 
-
-
-INFLUXDB_PKG=influxdb-latest-1.x86_64.rpm
-INFLUXDB_URL=http://s3.amazonaws.com/influxdb/$INFLUXDB_PKG
-
 GRAFANA_HOST=127.0.0.1
 GRAFANA_PORT=8080
-GRAFANA_VER=grafana-1.8.1
-GRAFANA_PKG=$GRAFANA_VER.tar.gz
-GRAFANA_URL=http://grafanarel.s3.amazonaws.com/$GRAFANA_PKG
 
 HTTP_USER=user
 HTTP_PASSWORD=changeit
 
 sudo yum -y update
 sudo yum history sync
-sudo yum -y install wget
+
 
 ###############################################################################################################################################
 #                                                                                                                                             #
@@ -36,8 +30,8 @@ sudo yum -y install wget
 
 #install influxdb
 #download
-wget $INFLUXDB_URL 
-sudo rpm -ivh $INFLUXDB_PKG
+curl -o /tmp/influxdb.rpm http://s3.amazonaws.com/influxdb/influxdb-latest-1.x86_64.rpm
+sudo rpm -ivh /tmp/influxdb.rpm
 #copy configuration
 sudo cp $INFLUXDB_CONFIG /opt/influxdb/shared/config.toml
 sudo chown influxdb:influxdb /opt/influxdb/shared/config.toml
@@ -55,8 +49,8 @@ sudo /etc/init.d/influxdb start
 #                                                                                                                                             #
 ###############################################################################################################################################
 #install grafana with nginx
-wget http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
-sudo rpm -ivh nginx-release-centos-6-0.el6.ngx.noarch.rpm
+curl -o /tmp/nginx-release-centos-7-0.el7.ngx.noarch.rpm http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+sudo rpm -ivh /tmp/nginx-release-centos-7-0.el7.ngx.noarch.rpm
 sudo yum -y install nginx
 
 #htpasswd
@@ -67,9 +61,8 @@ sudo htpasswd -dbc /etc/nginx/.htpasswd $HTTP_USER $HTTP_PASSWORD
 sudo rm /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/example_ssl.conf
 sudo sed -e "s|#GRAFANA_PORT#|$GRAFANA_PORT|g;s|#GRAFANA_HOST#|$GRAFANA_HOST|g" $NGINX_CONFIG -e  "w /etc/nginx/conf.d/grafana.conf"
 
-sudo chkconfig --level 2345 nginx on
 sudo chown nginx:nginx /etc/nginx/.htpasswd
-sudo service nginx start
+sudo systemctl start nginx.service
 
 
 ###############################################################################################################################################
@@ -78,11 +71,11 @@ sudo service nginx start
 #                                                                                                                                             #
 ###############################################################################################################################################
 # Download the latest grafana version
-wget $GRAFANA_URL
-sudo tar xvfz $GRAFANA_PKG -C /usr/share
+curl -o /tmp/grafana.tar.gz http://grafanarel.s3.amazonaws.com/grafana-1.8.1.tar.gz
+sudo tar xvfz /tmp/grafana.tar.gz -C /usr/share
 
 sudo mkdir -p /var/www/
-sudo ln  -s /usr/share/$GRAFANA_VER -d /var/www/grafana
+sudo ln  -s /usr/share/grafana-1.8.1 -d /var/www/grafana
 sudo sed -e "s|#SERVER_GRAFANA#|http://$GRAFANA_HOST:$GRAFANA_PORT|g" $GRAFANA_CONFIG -e  "w /var/www/grafana/config.js"
 sudo cp $GRAFANA_DASH /var/www/grafana/app/dashboards/dash.json
 sudo chown nginx:nginx /var/www/grafana/ -R
@@ -96,12 +89,12 @@ sudo semanage port -a -t http_port_t -p tcp 8086
 
 #Node JS
 sudo yum -y groupinstall "Development Tools"
-wget http://nodejs.org/dist/v0.10.33/node-v0.10.33-linux-x64.tar.gz
-sudo tar zxf node-v0.10.33-linux-x64.tar.gz -C /usr/local/
+curl -o /tmp/node-v0.10.33-linux-x64.tar.gz http://nodejs.org/dist/v0.10.33/node-v0.10.33-linux-x64.tar.gz
+sudo tar zxf /tmp/node-v0.10.33-linux-x64.tar.gz -C /usr/local/
 
 #statd
-wget https://github.com/etsy/statsd/archive/v0.7.2.tar.gz --no-check-certificate
-sudo tar zxf v0.7.2.tar.gz -C /usr/local
+curl -o /tmp/v0.7.2.tar.gz  https://github.com/etsy/statsd/archive/v0.7.2.tar.gz
+sudo tar zxf /tmp/v0.7.2.tar.gz -C /usr/local
 sudo cp $STATSD_CONFIG /usr/local/statsd-0.7.2/config.js
 /usr/local/node-v0.10.33-linux-x64/bin/node  /usr/local/statsd-0.7.2/stats.js  /usr/local/statsd-0.7.2/config.js &
 
